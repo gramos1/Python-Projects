ddb9e23cf26d716dc7bd924b69e19ed5b18270b9
gramos1 <49988419+gramos1@users.noreply.github.com>
2019-04-30 21:42:51 -0500

    Poe's_Raven_Scrape.py

--- poe_scrape.py
+++ poe_scrape.py
@@ -3,14 +3,11 @@ import requests
 import time
 import os
 import random
-os.system('color 0c')
+os.system('color 0c') # Setting the mood.
 os.system('cls')
 
 source = requests.get('http://www.gutenberg.org/files/17192/17192-h/17192-h.htm').text
 soup = BeautifulSoup(source, 'lxml')
-
-''' Using the find_all method in BeautifulSoup results in a ResultSet, so the only way to output
-any text from it you must print the value like if it was in a list and use the .text method '''
 
 poem = soup.find_all('div', class_='poem') # Here we are capturing the whole poem
 stanzas = soup.find_all('div', class_='stanza') # Here we are capturing the stanzas
@@ -30,5 +27,5 @@ def RandomStanza():
         exit()
         
 
-RandomStanza()
+RandomStanza() # Calling the function
 
